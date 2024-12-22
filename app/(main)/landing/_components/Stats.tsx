4d18dc61d63ffa45123e7c9e7fbc5fc34c45e3ab
younes <younesamari927@gmail.com>
2024-12-22 12:49:50 +0100

    landing page stats

--- app/(main)/landing/_components/Stats.tsx
+++ app/(main)/landing/_components/Stats.tsx
@@ -1,3 +1,23 @@
 export default function Stats() {
-  return <div className="mx-auto flex w-fit">Stats</div>;
+  return (
+    <div className={" flex items-center justify-center min-h screen cursor-pointer  "}>
+    <div className="flex flex-wrap gap-5 justify-center"> 
+
+    <div className="transition-transform transform hover:scale-105 duration-300 ease-in-out bg-black text-blue-400  border border-blue-500 p-6 rounded-lg text-center w-64 ">
+    <span className="text-3xl font-bold">999</span>
+      <p className="text-gray-300 mt-2">Communities</p>
+     </div>
+
+     <div className="transition-transform transform hover:scale-105 duration-300 ease-in-out bg-black text-blue-400  border border-blue-500 p-6 rounded-lg text-center w-64">
+     <span className="text-3xl font-bold">+9999</span>
+     <p className="text-gray-300 mt-2">Active Users</p>
+     </div>
+
+     <div className="transition-transform transform hover:scale-105 duration-300 ease-in-out bg-black text-blue-400  border border-blue-500 p-6 rounded-lg text-center w-64">
+     <span className="text-3xl font-bold">+9999</span>
+     <p className="text-gray-300 mt-2">posts</p>
+     </div>
+   </div>
+   </div>
+  )
 }
