0ecef467dfc3c9a538fa7c61afce4fe6fc062007
Xenos <acristyano22@gmail.com>
2024-12-24 03:48:12 -0800

    fix wierd layout issues in small screens and big screens

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,11 @@
 import Header from "@/layouts/Header";
-import {type ReactNode } from "react";
+import { type ReactNode } from "react";
 
-export default function layout({children}:Readonly<{children:ReactNode}>) {
+export default function layout({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <div className="container mx-auto px-4">
-    <Header />
-    <main>{children}</main>
+    <div className="container max-w-[1300px] mx-auto px-4">
+      <Header />
+      <main>{children}</main>
     </div>
   )
 }
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,8 +4,8 @@ import { type ReactNode } from "react";
 
 export default function layout({ children }: { children: ReactNode }) {
   return (
-    <div className="container mx-auto flex min-h-screen max-sm:flex-col">
-      <div className='w-fit'>
+    <div className="container mx-auto flex min-h-screen max-w-[1300px] max-sm:flex-col">
+      <div className="w-full max-lg:w-fit max-sm:w-full">
         <LeftNavBar />
       </div>
       {children}
