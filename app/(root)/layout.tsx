afe539aea8b7af0322d945b1e12841b9c867ad1b
Xenos <acristyano22@gmail.com>
2024-12-24 03:51:03 -0800

    add fixed width for the aside

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ import { type ReactNode } from "react";
 export default function layout({ children }: { children: ReactNode }) {
   return (
     <div className="container mx-auto flex min-h-screen max-w-[1300px] max-sm:flex-col">
-      <div className="w-full max-lg:w-fit max-sm:w-full">
+      <div className="w-[350px] max-lg:w-fit max-sm:w-full">
         <LeftNavBar />
       </div>
       {children}
