03abe82628cfdba69deeac98d41fb6c435cdaec1
Xenos <acristyano22@gmail.com>
2024-12-25 03:37:57 -0800

    add `mx-auto` with max-width

--- app/(main)/landing/_components/Hero.tsx
+++ app/(main)/landing/_components/Hero.tsx
@@ -3,7 +3,7 @@ import Typography from "@/components/ui/Typography";
 
 export default function Hero() {
   return (
-    <div className="flex h-[50vh] max-w-[1000px] flex-col items-center justify-center gap-8">
+    <div className="mx-auto flex h-[50vh] max-w-[1000px] flex-col items-center justify-center gap-8">
       <div className="mx-auto flex w-fit flex-col items-center gap-4">
         <Typography as="h1" variant="title" className="text-center">
           Explore More in Your Domain <br />
