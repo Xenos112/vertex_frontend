84512f828c43a75482acfd6d1181c1e391a14267
Xenos <acristyano22@gmail.com>
2024-12-25 03:31:10 -0800

    remove closing div

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,8 +5,7 @@ import { type ReactNode } from "react";
 export default function layout({ children }: { children: ReactNode }) {
   return (
     <div className="container mx-auto grid min-h-screen grid-cols-[25%_1fr_25%] max-lg:grid-cols-[auto_1fr] max-sm:grid-cols-1 max-sm:grid-rows-[auto_1fr] max-sm:flex-col">
-        <LeftNavBar />
-      </div>
+      <LeftNavBar />
       {children}
       <RightNacBar />
     </div>
