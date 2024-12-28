c78babe2adda09869bf16b7b176d4caf45739e95
Xenos <acristyano22@gmail.com>
2024-12-28 02:34:18 -0800

    add pracing page

---
+++ app/(main)/premium/_components/Pricing.tsx
@@ -0,0 +1,101 @@
+"use client";
+import { createContext, use, type ReactNode } from "react";
+import { Check, Minus } from "lucide-react";
+import cn from "@/utils/cn";
+
+export type PricingData = {
+  name: string;
+  price: number;
+  type: "month" | "year";
+  features: {
+    description: string;
+    available: boolean;
+  }[];
+};
+
+const data: PricingData = {
+  name: "Free Tier",
+  price: 5,
+  type: "month",
+  features: [
+    {
+      description: "Unlimited posts per month",
+      available: true,
+    },
+    {
+      description: "Unlimited posts per month",
+      available: true,
+    },
+    {
+      description: "Unlimited posts per month",
+      available: true,
+    },
+    {
+      description: "Unlimited posts per month",
+      available: true,
+    },
+  ],
+};
+
+const PricingContext = createContext<PricingData>(data);
+
+export function Pricing({
+  data,
+  children,
+  className,
+}: {
+  data: PricingData;
+  className?: string;
+  children: ReactNode;
+}) {
+  return (
+    <PricingContext value={data}>
+      <div
+        className={cn(
+          "relative flex h-[70dvh] flex-1 flex-col gap-8 rounded-lg border-4 border-primary p-4",
+          className,
+        )}
+      >
+        {children}
+      </div>
+    </PricingContext>
+  );
+}
+
+export function PricingHeader() {
+  const pricing = use(PricingContext);
+  return (
+    <div className="flex flex-col items-center gap-3">
+      <h1 className="text-[30px] font-semibold">{pricing.name}</h1>
+      <h2 className="text-[40px] font-semibold">
+        {pricing.price}$/{pricing.type}
+      </h2>
+    </div>
+  );
+}
+
+export function PricingBody() {
+  const pricing = use(PricingContext);
+  return (
+    <div>
+      <ul className="flex flex-col gap-3">
+        {pricing.features.map((feature) => (
+          <li key={feature.description} className="flex items-center gap-3">
+            <span className={`${feature.available || "text-gray-700"}`}>
+              {feature.available ? <Check /> : <Minus />}
+            </span>
+            <span className={`${feature.available || "text-gray-700"}`}>{feature.description}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export function PricingChip({ children }: { children: ReactNode }) {
+  return (
+    <span className="absolute left-1/2 z-40 -translate-x-1/2 -translate-y-[calc(100%-4px)] rounded-full bg-primary px-5 py-3 font-semibold text-white">
+      {children}
+    </span>
+  );
+}
---
+++ app/(main)/premium/page.tsx
@@ -0,0 +1,101 @@
+import Button from "@/components/ui/Button";
+import {
+  Pricing,
+  PricingBody,
+  PricingHeader,
+  type PricingData,
+  PricingChip,
+} from "./_components/Pricing";
+
+const freeTier: PricingData = {
+  name: "Free Tier",
+  price: 0,
+  type: "month",
+  features: [
+    {
+      description: "Earn Subscription Badge",
+      available: true,
+    },
+    {
+      description: "Search Boost",
+      available: true,
+    },
+    {
+      description: "Create 3 Custom Communities",
+      available: false,
+    },
+  ],
+};
+
+// TODO:add more features
+
+const proTier: PricingData = {
+  name: "Free Tier",
+  price: 5,
+  type: "month",
+  features: [
+    {
+      description: "Earn Subscription Badge",
+      available: true,
+    },
+    {
+      description: "Create 3 Custom Communities",
+      available: true,
+    },
+    {
+      description: "Search Boost",
+      available: true,
+    },
+  ],
+};
+
+const maxTier: PricingData = {
+  name: "Free Tier",
+  price: 20,
+  type: "month",
+  features: [
+    {
+      description: "Earn Subscription Badge",
+      available: true,
+    },
+    {
+      description: "Create 10 Custom Communities",
+      available: true,
+    },
+    {
+      description: "Search Boost",
+      available: true,
+    },
+  ],
+};
+
+export default function PremiumPage() {
+  return (
+    <main className="h-[calc(100vh-80px)]">
+      <div className="flex h-full items-center justify-center gap-7">
+        <Pricing className="scale-95" data={freeTier}>
+          <PricingHeader />
+          <PricingBody />
+          <Button size="lg" className="mt-auto">
+            Subscribe
+          </Button>
+        </Pricing>
+        <Pricing className="scale-110" data={proTier}>
+          <PricingHeader />
+          <PricingBody />
+          <Button size="lg" className="mt-auto">
+            Subscribe
+          </Button>
+        </Pricing>
+        <Pricing className="scale-95" data={maxTier}>
+          <PricingHeader />
+          <PricingBody />
+          <PricingChip>Best Deal</PricingChip>
+          <Button size="lg" className="mt-auto">
+            Subscribe
+          </Button>
+        </Pricing>
+      </div>
+    </main>
+  );
+}
