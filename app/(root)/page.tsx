83ceeb9ac33136dfa2c62fd3923986fbcec33e41
Xenos <acristyano22@gmail.com>
2025-01-17 06:16:58 -0800

    add fallback icon if the user is not logged in

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,21 +9,35 @@ import {
 import Input from "@/components/ui/Input";
 import Button from "@/components/ui/Button";
 import CreatePostModal from "@/features/post/components/CreatePostModal";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { User } from "lucide-react";
 
 export default function page() {
   const user = useUserStore((state) => state.user);
+  const pathName = usePathname()
+
 
   return (
     <main className="w-full">
-      <div></div>
+    {/* TODO: make the selected tab active and more emphesised */}
+      <div className="w-full flex gap-4 p-4 py-[21px] items-center justify-center unerline">
+        <Link href={`${pathName}?t=feed`}>
+          For You
+        </Link>
+        <Link href={`${pathName}?t=communities`}>Communities</Link>
+      </div>
       <Modal>
         <ToggleButton className="w-full">
           <div className="flex items-center justify-between border-y border-grayish px-6 py-[15px]">
-            <div className="flex gap-4">
-              <Avatar>
-                <AvatarImage src={user?.image_url!} alt={user?.user_name} />
-                <AvatarFallback>{user?.user_name}</AvatarFallback>
-              </Avatar>
+            <div className="flex gap-4 items-center">
+              {user ? (
+                <Avatar className="w-10 h-10">
+                  <AvatarImage src={user?.image_url!} alt={user?.user_name} />
+                  <AvatarFallback>{user?.user_name}</AvatarFallback>
+                </Avatar>
+                ): <User size={30} />
+              }
               <Input placeholder="What's on your mind?" className="w-full bg-transparent" />
             </div>
             <Button variant="outline" size={"sm"}>
