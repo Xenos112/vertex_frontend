761c467350bbcea4248fd8ce16d88fbb7c9f3adc
Xenos112 <acristyano22@gmail.com>
2025-01-18 04:36:42 -0800

    wrap the get post method in try catch block

--- features/post/api/get-post.ts
+++ features/post/api/get-post.ts
@@ -4,22 +4,26 @@ import vertex from "../../../api";
 type Response = Post | { error: string };
 
 export default async function getPost(id: string): Promise<Response> {
-  const data = await vertex.get<{ data: Post }>(`http://localhost:4000/post/${id}`, {
-    credentials: "include",
-    throwHttpErrors: false,
-  });
-  if (data.status === 404) {
-    return { error: "Post not found" };
-  }
+  try {
+    const data = await vertex.get<{ data: Post }>(`http://localhost:4000/post/${id}`, {
+      credentials: "include",
+      throwHttpErrors: false,
+    });
+    if (data.status === 404) {
+      return { error: "Post not found" };
+    }
 
-  if (data.status === 400) {
-    return { error: "please enter a valid id for the post" };
-  }
+    if (data.status === 400) {
+      return { error: "please enter a valid id for the post" };
+    }
 
-  if (data.status === 500 || data.status !== 200) {
+    if (data.status === 500 || data.status !== 200) {
+      return { error: "something went wrong" };
+    }
+
+    const post = (await data.json()).data;
+    return post;
+  } catch (error) {
     return { error: "something went wrong" };
   }
-
-  const post = (await data.json()).data;
-  return post;
 }
